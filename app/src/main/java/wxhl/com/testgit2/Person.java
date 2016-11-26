bfe0e4d9f5b6b1794cc1dd86dafdbcb8f26d2801
liuhongchen <1664691399@qq.com>
2016-11-26 17:22:12 +0800

    修改了Person，添加了name、sex字段

--- app/src/main/java/wxhl/com/testgit2/Person.java
+++ app/src/main/java/wxhl/com/testgit2/Person.java
@@ -6,5 +6,23 @@ package wxhl.com.testgit2;
 
 public class Person {
 
-    private int age;
+//    private int age;
+    private int sex;
+    private String name;
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
